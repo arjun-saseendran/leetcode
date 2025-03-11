cc65f24a15b04c08452589d1a7204ceaa34ba810
arjun-saseendran <arjunsaseendran@icloud.com>
2025-03-11 22:15:51 +0530

    add 49-2114.js

---
+++ 49-2114.js
@@ -0,0 +1,13 @@
+const mostWordsFound = (sentences) => {
+    let arr = [];
+for(let i = 0; i < sentences.length; i++){
+    arr.push(sentences[i].split(' ').length);
+
+
+}
+const sortedArr = arr.sort((a,b) => b-a)
+return sortedArr[0]
+}
+
+const result = mostWordsFound(["w jrpihe zsyqn l dxchifbxlasaehj","nmmfrwyl jscqyxk a xfibiooix xolyqfdspkliyejsnksfewbjom","xnleojowaxwpyogyrayfgyuzhgtdzrsyococuqexggigtberizdzlyrdsfvryiynhg","krpwiazoulcixkkeyogizvicdkbrsiiuhizhkxdpssynfzuigvcbovm","rgmz rgztiup wqnvbucfqcyjivvoeedyxvjsmtqwpqpxmzdupfyfeewxegrlbjtsjkusyektigr","o lgsbechr lqcgfiat pkqdutzrq iveyv iqzgvyddyoqqmqerbmkxlbtmdtkinlk","hrvh efqvjilibdqxjlpmanmogiossjyxepotezo","qstd zui nbbohtuk","qsdrerdzjvhxjqchvuewevyzlkyydpeeblpc"])
+console.log(result);
